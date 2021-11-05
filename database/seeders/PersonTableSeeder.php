92529ac1ce51fed5ecb28e516eaa0f81a240cefb
gboumphrey <74275873+gboumphrey@users.noreply.github.com>
2021-11-05 00:23:48 +0000

    post and person models, person seeder

---
+++ database/seeders/PersonTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Person;
+
+class PersonTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $me = new Person;
+        $me->name = 'George';
+        $me->email = 'george@email.com';
+        $me->save();
+
+    }
+}
